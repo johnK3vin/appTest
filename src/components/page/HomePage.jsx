c127e7e24cc75106b35db7a897ea0e96235a11d5
johnkevin <johnkevinvegaolivo@gmail.com>
2023-09-13 16:46:32 -0300

    rutas de imagenes4

--- src/components/page/HomePage.jsx
+++ src/components/page/HomePage.jsx
@@ -1,18 +1,15 @@
 import { Link } from "react-router-dom"
 const HomePage = () => {
-  const rutasImg = {
-    miedo : '/appTest/imgTestAnimal/miedo.jpg',
-    totem: '/appTest/public/imgTestAnimal/totemanimal.jpg'
-  }
+
   return (
     <div className="homeContainer">
-        <Link to='/test/emocional mente sano' className="Test">
+        <Link to='/test/emocional mente sano' className="Test-sano">
             <div className="divTest">¿Eres Emocionalmente Sano?</div>
         </Link>
-        <Link to='/test/revela tu miedo' className="Test" style={{backgroundImage: `url(${rutasImg.miedo})`}}>
+        <Link to='/test/revela tu miedo' className="Test-miedo" style={{backgroundImage: "url('/appTest/imgTestAnimal/miedo.jpg')"}}>
             <div className="divTest">Revela tu miedo mas profundo</div>
         </Link>
-        <Link to='/test/decubre animal totem' className="Test" style={{backgroundImage: `url(${rutasImg.totem})`}}>
+        <Link to='/test/decubre animal totem' className="Test-totem" style={{backgroundImage: "url('/appTest/public/imgTestAnimal/totemanimal.jpg')"}}>
           <div className="divTest">Descubre tu animal totem</div>
         </Link>
     </div>
