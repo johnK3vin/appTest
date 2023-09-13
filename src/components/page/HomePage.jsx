caeaaff2bf3494703b26404fd553d9b511153582
johnkevin <johnkevinvegaolivo@gmail.com>
2023-09-13 16:56:31 -0300

    rutas de imagenes5

--- src/components/page/HomePage.jsx
+++ src/components/page/HomePage.jsx
@@ -6,10 +6,10 @@ const HomePage = () => {
         <Link to='/test/emocional mente sano' className="Test-sano">
             <div className="divTest">¿Eres Emocionalmente Sano?</div>
         </Link>
-        <Link to='/test/revela tu miedo' className="Test-miedo" style={{backgroundImage: "url('/appTest/imgTestAnimal/miedo.jpg')"}}>
+        <Link to='/test/revela tu miedo' className="Test-miedo">
             <div className="divTest">Revela tu miedo mas profundo</div>
         </Link>
-        <Link to='/test/decubre animal totem' className="Test-totem" style={{backgroundImage: "url('/appTest/public/imgTestAnimal/totemanimal.jpg')"}}>
+        <Link to='/test/decubre animal totem' className="Test-totem">
           <div className="divTest">Descubre tu animal totem</div>
         </Link>
     </div>
